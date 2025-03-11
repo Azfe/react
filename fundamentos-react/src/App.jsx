b1bcdef19c1c9ec08f363004b9aabda5084f5e45
Alex Zapata <alexzapata1984@gmail.com>
2025-03-11 22:11:25 +0100

    Add ternary operator

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,14 +1,35 @@
 import { Fragment } from 'react';
 
+const MyButton = () => {
+  return <button>Click me!</button>
+}
+
+const OnlineText = () => {
+  return (
+    <p>Usuario logueado</p>    
+  ) 
+}
+
+const OfflineText = () => {
+  return (
+    <p>Usuario no logueado</p>    
+  ) 
+}
+
 const title = 'React is awesome!';
 const classTitle = 'text-center';
 const pathImg = 'https://picsum.photos/200/300';
+const user = true;
 
 const App = () => {
   return (
     <Fragment>
       <h1 className={ classTitle }>{ title.toUpperCase() }</h1>
-      <img src={ pathImg } alt={`imagen: ${title}`} />      
+      <img src={ pathImg } alt={`imagen: ${title}`} />
+      <MyButton />
+      {
+        user ? <OnlineText/> : <OfflineText/>
+      }
     </Fragment>
   )
 }
