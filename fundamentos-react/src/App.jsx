902fae061998e1a4e1934169c3587c89a0a0f359
Alex Zapata <alexzapata1984@gmail.com>
2025-03-11 19:43:20 +0100

    Add JSX + Fragment + Interpolation

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,16 @@
+import { Fragment } from 'react';
+
+const title = 'React is awesome!';
+const classTitle = 'text-center';
+const pathImg = 'https://picsum.photos/200/300';
+
 const App = () => {
-  return <h1>React is awesome!</h1>
+  return (
+    <Fragment>
+      <h1 className={ classTitle }>{ title.toUpperCase() }</h1>
+      <img src={ pathImg } alt={`imagen: ${title}`} />      
+    </Fragment>
+  )
 }
 
 export default App;
