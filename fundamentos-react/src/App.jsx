fe1e1efdafb4c0edf568aee8ce6468f7736908d1
Alex Zapata <alexzapata1984@gmail.com>
2025-03-12 10:29:16 +0100

    Add reuse components + import static files

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import imgFakoly from './assets/images/folder.jpg';
 
 const MyButton = () => {
   return <button>Click me!</button>
@@ -18,17 +19,20 @@ const OfflineText = () => {
 
 const title = 'React is awesome!';
 const classTitle = 'text-center';
-const pathImg = 'https://picsum.photos/200/300';
+//const pathImg = 'src/assets/images/folder.jpg';
 const user = true;
 
 const App = () => {
   return (
     <Fragment>
       <h1 className={ classTitle }>{ title.toUpperCase() }</h1>
-      <img src={ pathImg } alt={`imagen: ${title}`} />
+      <img src={ imgFakoly } alt={`imagen: ${title}`} className="image"/>
       <MyButton />
       {
         user ? <OnlineText/> : <OfflineText/>
+      }
+      {
+        user && <OnlineText/>
       }
     </Fragment>
   )
