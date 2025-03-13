44ff71b7ecbcc51c0e806b16831bda7318b6161d
Alex Zapata <alexzapata1984@gmail.com>
2025-03-13 11:30:34 +0100

    Add Structure Form JSX + onSubmit + useRef

--- forms-react-project/src/components/NoControlado.jsx
+++ forms-react-project/src/components/NoControlado.jsx
@@ -1,5 +1,35 @@
+import { useRef } from "react";
+
 const NoControlado = () => {    
-    return 'NoControlado';
+
+    const form = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviando formulario...");
+        console.log(form.current);
+        /*
+        const data = {
+            title: e.target.title.value,
+            description: e.target.description.value,
+            state: e.target.state.value
+        };
+        console.log(data);
+        e.target.reset();
+        */
+    };
+
+    return (
+        <form onSubmit={handleSubmit} ref={form}>
+            <input name="title" type="text" placeholder="Ingrese todo" className="form-control mb-2"/>
+            <textarea name="description" className="form-control mb-2" placeholder="Ingrese descripción" />
+            <select name="state" defaultValue="pendiente" className="form-select mb-2">
+                <option value="pendiente">Pendiente</option>
+                <option value="completado">Competado</option>
+            </select>
+            <button className="btn btn-primary" type="submit">Procesar</button>            
+        </form>        
+    );
 };
 
 export default NoControlado;
