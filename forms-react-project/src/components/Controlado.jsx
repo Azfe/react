a68de6957e59c8fd0c7dcd83b931eb00ec18a5d6
Alex Zapata <alexzapata1984@gmail.com>
2025-03-14 11:44:24 +0100

    Add onChange

--- forms-react-project/src/components/Controlado.jsx
+++ forms-react-project/src/components/Controlado.jsx
@@ -1,24 +1,45 @@
+import React, { useState } from 'react';
 
-
-const Controlado = () => {    
+const Controlado = () => { 
+    
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [state, setState] = useState('pendiente');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
-
-        // Enviar los datos
+        console.log(title, description, state);
     };
 
     return (
-        <form onSubmit={handleSubmit} ref={form}>
-            <input name="title" type="text" placeholder="Ingrese todo" className="form-control mb-2"/>
-            <textarea name="description" className="form-control mb-2" placeholder="Ingrese descripción" />
-            <select name="state" defaultValue="pendiente" className="form-select mb-2">
+        <form onSubmit={handleSubmit}>
+            <input
+                name="title" 
+                type="text" 
+                placeholder="Ingrese To do" 
+                value={title} 
+                className="form-control mb-2"
+                onChange={e => setTitle(e.target.value)}
+            />
+            <textarea 
+                name="description" 
+                className="form-control mb-2" 
+                placeholder="Ingrese descripción" 
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+            />
+            <select 
+                name="state" 
+                className="form-select mb-2" 
+                value={state} 
+                onChange={e => setState(e.target.value)}
+            >
                 <option value="pendiente">Pendiente</option>
                 <option value="completado">Competado</option>
             </select>
-            <button className="btn btn-primary" type="submit">Procesar</button> 
-            {error && <div className="alert alert-danger mt-2">{error}</div>}           
+            <button className="btn btn-primary" type="submit">
+                Procesar
+            </button>                     
         </form>        
     );
 };
