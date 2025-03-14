79815c1a787cf580c25c4a2063a58bafd8f592d2
Alex Zapata <alexzapata1984@gmail.com>
2025-03-14 11:45:37 +0100

    Modify content App.jsx: add form titles + controlled form

--- forms-react-project/src/App.jsx
+++ forms-react-project/src/App.jsx
@@ -1,10 +1,14 @@
 import NoControlado from "./components/NoControlado";
+import Controlado from "./components/Controlado";
 
 const App = () => {
   return (
     <div className="container">
       <h1>Formularios</h1>
+      <h2>Formulario no controlado</h2>
       <NoControlado />
+      <h2>Formulario controlado</h2>
+      <Controlado />
       
     </div>
   );
