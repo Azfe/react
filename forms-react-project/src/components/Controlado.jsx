fd3a5dc1f355d6d5d5521a2931a8ca3073d25f55
Alex Zapata <alexzapata1984@gmail.com>
2025-03-14 12:09:49 +0100

    Add useState with objects

--- forms-react-project/src/components/Controlado.jsx
+++ forms-react-project/src/components/Controlado.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 
 const Controlado = () => { 
-    
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [state, setState] = useState('pendiente');
+
+    const [task, setTask] = useState({
+        title: '',
+        description: '',
+        state: 'pendiente'
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title, description, state);
+        console.log(task.title, description, state);
     };
 
     return (
@@ -16,23 +18,23 @@ const Controlado = () => {
             <input
                 name="title" 
                 type="text" 
-                placeholder="Ingrese To do" 
-                value={title} 
+                placeholder="Ingrese tarea" 
+                value={task.title} 
                 className="form-control mb-2"
-                onChange={e => setTitle(e.target.value)}
+                onChange={e => setTask({...task, title: e.target.value})}
             />
             <textarea 
                 name="description" 
                 className="form-control mb-2" 
                 placeholder="Ingrese descripción" 
-                value={description}
-                onChange={e => setDescription(e.target.value)}
+                value={task.description}
+                onChange={e => setTask({...task, description: e.target.value})}
             />
             <select 
                 name="state" 
                 className="form-select mb-2" 
-                value={state} 
-                onChange={e => setState(e.target.value)}
+                value={task.state}
+                onChange={e => setTask({...task, state: e.target.value})}
             >
                 <option value="pendiente">Pendiente</option>
                 <option value="completado">Competado</option>
