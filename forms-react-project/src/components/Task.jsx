8fc0a4948fc61b50316c583e58f9457f1986a44d
Alex Zapata <alexzapata1984@gmail.com>
2025-03-15 16:40:57 +0100

    Add Read Tasks

---
+++ forms-react-project/src/components/Task.jsx
@@ -0,0 +1,24 @@
+const Task = ({ task }) => {
+
+    const { id, title, description, priority, state } = task;
+
+    return (
+        <li className="list-group-item">
+            <div className="d-flex justify-content-between align-items-start">
+                <div>
+                    <h5 className={`${state && 'text-decoration-line-through'}`}>{title}</h5>
+                    <p className={`${description && 'text-decoration-line-through'}`}>{description}</p>
+                    <div className="d-flex gap-2">
+                        <button className="btn btn-sm btn-danger">Eliminar</button>
+                        <button className="btn btn-sm btn-warning">Actualizar</button>
+                    </div>
+                </div>
+                <span className="badge text-bg-primary">
+                    {priority && 'Prioritario'}
+                </span>
+            </div>
+        </li>
+    )
+};
+
+export default Task;
--- forms-react-project/src/components/Tasks.jsx
+++ forms-react-project/src/components/Tasks.jsx
@@ -1,15 +1,12 @@
+import Task from "./Task";
+
 const Tasks = ({tasks}) => {
   return (
     <div className="mt-4">
       <h2 className="my-4">Tareas</h2>
-      <ul>
+      <ul className="list-group">
         {tasks.map(task => (
-          <li key={task.id}>
-            <h5>{task.title}</h5>
-            <p>{task.description}</p>
-            <p>{task.priority ? 'Prioritaria' : 'No prioritaria'}</p>
-            <p>{task.state ? 'Completada' : 'Pendiente'}</p>
-          </li>
+          <Task key={task.id} task={task} />
         ))}
       </ul>
     </div>
