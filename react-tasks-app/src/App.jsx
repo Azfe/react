66c61e8ea4fd7c44f142f3b19e1774dd3340c96c
Alex Zapata <alexzapata1984@gmail.com>
2025-03-22 18:44:11 +0100

    Add Form

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -9,8 +9,13 @@ const App = () => {
               Luna
             </button>
           </div>
-          <form action="">
-            <input type="text" placeholder="Create a new task..."/>
+          <form action="" className="flex bg-white rounded-lg px-4 py-4 mt-4 overflow-hidden gap-4 items-center">
+            <span className="w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></span>
+            <input 
+              type="text" 
+              placeholder="Create a new task..." 
+              className="w-full text-gray-neutral outline-none"        
+            />
           </form>
         </header>
 
