23125cfa7bd6bdad337f9467c96db200baeb18a5
Alex Zapata <alexzapata1984@gmail.com>
2025-03-22 17:09:31 +0100

    Add semantic structure

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -2,13 +2,46 @@ const App = () => {
   return (
     <>
       <div className="bg-[url('./src/assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-center bg-cover">
-        <h1 className="uppercase">Todo</h1>
-        <button>
-          Luna
-        </button>
-        
-      </div>
-    
+        <header className="container mx-auto px-4">
+          <h1 className="uppercase">Todo</h1>
+          <button>
+            Luna
+          </button>
+          <form action="">
+            <input type="text" placeholder="Create a new task..."/>
+          </form>
+        </header>
+
+        <main className="container mx-auto px-4">
+          <article>
+            <button>Círculo</button>
+            <p>Complete online Javascript course</p>
+            <button>exis</button>
+          </article>
+          <article>
+            <button>Círculo</button>
+            <p>Complete online Javascript course</p>
+            <button>exis</button>
+          </article>
+          <article>
+            <button>Círculo</button>
+            <p>Complete online Javascript course</p>
+            <button>exis</button>
+          </article>
+          <section>
+            <span>5 items left</span>
+            <button>Clear completed</button>
+          </section>
+        </main>
+
+        <section className="container mx-auto px-4">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </section>
+
+        <p className="text-center">Drag and drop to reaorder list</p>
+      </div>   
     
     </>
   );
