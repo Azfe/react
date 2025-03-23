e9af1d10b6c2f1834ae567721e1520b9c61796b5
Alex Zapata <alexzapata1984@gmail.com>
2025-03-23 11:13:27 +0100

    Add Article + Flexbox

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -22,32 +22,33 @@ const App = () => {
         </header>
 
         <main className="container mx-auto mt-8 px-4">
-          <div className="bg-white rounded-md px-4">
-          <article>
-            <button className="w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
-            <p className="text-gray-600">Complete online Javascript course</p>
-            <button>
-              <IconCross />
-            </button>
-          </article>
-          <article>
-            <button className="w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
-            <p className="text-gray-600">Complete online Javascript course</p>
-            <button>
-              <IconCross />
-            </button>
-          </article>
-          <article>
-            <button className="w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
-            <p className="text-gray-600">Complete online Javascript course</p>
-            <button>
-              <IconCross />
-            </button>
-          </article>
-          <section>
-            <span>5 items left</span>
-            <button>Clear completed</button>
-          </section>
+          <div className="flex-row gap-4 mt-2 bg-white rounded-md">
+            <article className="flex gap-4 px-4 py-4 items-center border-b border-b-gray-400">
+              <button className="flex-none w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
+              <p className="text-gray-600 grow">Complete online Javascript course</p>
+              <button className="flex-none ml-auto">
+                <IconCross />
+              </button>
+            </article>
+            <article className="flex gap-4 px-4 py-4 items-center border-b border-b-gray-400">
+              <button className="flex-none w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
+              <p className="text-gray-600 grow">Complete online Javascript course</p>
+              <button className="flex-none ml-auto">
+                <IconCross />
+              </button>
+            </article>
+            <article className="flex gap-4 px-4 py-4 items-center border-b border-b-gray-400">
+              <button className="flex-none w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
+              <p className="text-gray-600 grow">Complete online Javascript course</p>
+              <button className="flex-none ml-auto">
+                <IconCross />
+              </button>
+            </article>
+            
+            <section>
+              <span>5 items left</span>
+              <button>Clear completed</button>
+            </section>
           </div>          
         </main>
 
