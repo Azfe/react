98905139d16aed1e100e18d215aea524c446d206
Alex Zapata <alexzapata1984@gmail.com>
2025-03-23 12:52:42 +0100

    Add Rest operator, spreed operator

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -1,4 +1,5 @@
 import IconCross from "./components/icons/IconCross";
+import IconMoon from "./components/icons/IconMoon";
 
 const App = () => {
   return (
@@ -34,7 +35,7 @@ const App = () => {
               <button className="flex-none w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
               <p className="text-gray-600 grow">Complete online Javascript course</p>
               <button className="flex-none ml-auto">
-                <IconMoon />
+                <IconMoon className="fill-red-600" />
               </button>
             </article>
             <article className="flex gap-4 items-center border-b border-b-gray-400">
