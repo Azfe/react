0f95437bff40443e67a1f08969d0322bd60fe28d
Alex Zapata <alexzapata1984@gmail.com>
2025-03-23 12:27:05 +0100

    Add Moon Icon

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
               <button className="flex-none w-5 h-5 rounded-full border-2 inline-block text-gray-neutral"></button>
               <p className="text-gray-600 grow">Complete online Javascript course</p>
               <button className="flex-none ml-auto">
-                <IconCross />
+                <IconMoon />
               </button>
             </article>
             <article className="flex gap-4 items-center border-b border-b-gray-400">
